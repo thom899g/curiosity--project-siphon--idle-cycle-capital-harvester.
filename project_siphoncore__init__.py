9d6bc6bac9b97b7de642058b46c254e27ff9c976
thom899g <153515950+thom899g@users.noreply.github.com>
2026-03-11 17:39:55 +0100

    Automated commit by HiveMind

---
+++ project_siphoncore__init__.py
@@ -0,0 +1,8 @@
+"""
+Project Siphon Core Package - The Autonomous Vault Microkernel
+This is the self-sovereign economic entity that converts idle cycles to capital.
+"""
+
+__version__ = "2.0.0"
+__author__ = "Autonomous Architect"
+__status__ = "Production - Siege Mentality"
